f833f9add264d8c29ee0e4dfec4c5a895ea4fa31
mykyta <mykyta.sidorov@kse.org.ua>
2026-03-28 15:55:48 +0200

    Stage 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,25 @@
-﻿
+﻿namespace Week8;
+using System;
+
 class Program
 {
     public static void Main()
     {
-        
+        var file = new FileResource("report.txt");
+        var network = new NetworkResource("api.company.local");
+        var manager = new ResourceManager<Resource>();
+
+        manager.Add(file);
+        manager.Add(network);
+        manager.OpenAll();
+
+        using (var res = new FileResource("HW3.txt"))
+        {
+            res.Open();
+        }
+        manager.CloseAll();
+
+        Console.WriteLine("Done.");
     }
     
 }
