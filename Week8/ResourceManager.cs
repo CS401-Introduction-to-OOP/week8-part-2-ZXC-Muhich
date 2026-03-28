528dad8396e8dcce1fe53ad19f37629c1098552b
mykyta <mykyta.sidorov@kse.org.ua>
2026-03-28 15:50:54 +0200

    Stage 2

---
+++ Week8/ResourceManager.cs
@@ -0,0 +1,28 @@
+﻿namespace Week8;
+using System.Collections.Generic;
+
+public class ResourceManager<T> where T : Resource
+{
+    private readonly List<T> _resources = new();
+
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+    }
+
+    public void OpenAll()
+    {
+        for (int i = 0; i < _resources.Count; i++)
+        {
+            _resources[i].Open();
+        }
+    }
+
+    public void CloseAll()
+    {
+        for (int i = 0; i < _resources.Count; i++)
+        {
+            _resources[i].Close();
+        }
+    }
+}
