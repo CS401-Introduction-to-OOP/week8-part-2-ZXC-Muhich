50d11ecf875ecae38a0fb4ed0524d701b9a85dbf
mykyta <mykyta.sidorov@kse.org.ua>
2026-03-28 16:06:19 +0200

    Stage 2

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,20 @@
+﻿namespace Week8;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;
+        object b = a;
+        int c = (int)b;
+
+        // a is stored as an int
+        // b stores a boxed copy of a
+        // c gets the value after unboxing
+        // get all values and print them
+
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+    }
+}
