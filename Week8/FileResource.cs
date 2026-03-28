8cd631df05b2b570698cddfa47f9ee73132276a4
mykyta <mykyta.sidorov@kse.org.ua>
2026-03-28 15:42:26 +0200

    1 stage

---
+++ Week8/FileResource.cs
@@ -0,0 +1,27 @@
+﻿namespace Week8;
+using System;
+
+public class FileResource : Resource, IDisposable
+{
+    public FileResource(string name) : base(name)
+    {
+    }
+
+    public override void Open()
+    {
+        IsOpen = true;
+        Console.WriteLine("File opened: " + Name);
+    }
+
+    public override void Close()
+    {
+        IsOpen = false;
+        Console.WriteLine("File closed: " + Name);
+    }
+
+    public void Dispose()
+    {
+        Close();
+        Console.WriteLine("File disposed: " + Name);
+    }
+}
---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,27 @@
+﻿namespace Week8;
+using System;
+
+public class NetworkResource : Resource, IDisposable
+{
+    public NetworkResource(string name) : base(name)
+    {
+    }
+
+    public override void Open()
+    {
+        IsOpen = true;
+        Console.WriteLine("Network is open: " + Name);
+    }
+
+    public override void Close()
+    {
+        IsOpen = false;
+        Console.WriteLine("Network is closed: " + Name);
+    }
+
+    public void Dispose()
+    {
+        Close();
+        Console.WriteLine("Network disposed: " + Name);
+    }
+}
