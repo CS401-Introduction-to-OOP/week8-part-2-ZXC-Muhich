3d6536cdc02b236106246f5a3507fec1ec581ad2
mykyta <mykyta.sidorov@kse.org.ua>
2026-03-28 16:21:18 +0200

    Stage 2

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,24 @@
+﻿namespace Week8;
+using System.Collections.Generic;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public long GetTotalSize()
+    {
+        long total = 0;
+
+        for (int i = 0; i < _items.Count; i++)
+        {
+            total += _items[i].GetSize();
+        }
+
+        return total;
+    }
+}
